2c4a0a844625cd51b77610aea80a6ad51338a3b1
Saraswathi560 <saraswathiabhi29@gmail.com>
2025-10-26 12:34:40 +0530

    added a program

--- abc.js
+++ abc.js
@@ -1,1 +1,7 @@
 console.log("This is abc.js in master branch.");
+var pullFlag = true;
+if (pullFlag) {
+    console.log("pull flag is enabled.");
+} else {
+    console.log("pull flag is disabled.");
+}
